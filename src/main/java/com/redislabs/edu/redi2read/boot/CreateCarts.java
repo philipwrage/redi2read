171155ad262a6a3a04eb93e8a4b65288370a28b7
Philip Wrage <philipwrage@users.noreply.github.com>
2021-04-24 04:10:22 -0500

    feature: create random set of carts

--- src/main/java/com/redislabs/edu/redi2read/boot/CreateCarts.java
+++ src/main/java/com/redislabs/edu/redi2read/boot/CreateCarts.java
@@ -1,5 +1,9 @@
 package com.redislabs.edu.redi2read.boot;
 
+import com.redislabs.edu.redi2read.models.Book;
+import com.redislabs.edu.redi2read.models.Cart;
+import com.redislabs.edu.redi2read.models.CartItem;
+import com.redislabs.edu.redi2read.models.User;
 import com.redislabs.edu.redi2read.repositories.BookRepository;
 import com.redislabs.edu.redi2read.repositories.CartRepository;
 import com.redislabs.edu.redi2read.services.CartService;
@@ -9,6 +13,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+import java.util.stream.IntStream;
 
 @Component
 @Order( 5 )
@@ -35,6 +43,48 @@ public class CreateCarts implements CommandLineRunner {
 
     @Override
     public void run( String... args ) throws Exception {
+        if ( cartRepository.count() == 0 ) {
+            Random random = new Random();
 
+            IntStream.range( 0, numberOfCarts )
+                .forEach( n -> {
+                    String userId = redisTemplate.opsForSet().randomMember( User.class.getName() );
+                    Set<Book> books = getRandomBooks( bookRepository, 7 );
+
+                    Cart cart = Cart.builder().userId( userId ).build();
+                    cart.setCartItems( getCartItemsForBooks(books) );
+                    cartRepository.save( cart );
+
+                    if ( random.nextBoolean() ) {
+                        cartService.checkOut( cart.getId() );
+                    }
+                } );
+            log.debug( "Created carts." );
+        }
+    }
+
+    private Set<Book> getRandomBooks( BookRepository bookRepository, int max ) {
+        Random random = new Random();
+        int count = random.nextInt( max ) + 1;
+        Set<Book> books = new HashSet<>();
+        IntStream.range( 0, count ).forEach( n -> {
+            String bookId = redisTemplate.opsForSet().randomMember( Book.class.getName() );
+            bookRepository.findById( bookId ).ifPresent( books::add );
+        } );
+        return books;
+    }
+
+    private Set<CartItem> getCartItemsForBooks( Set<Book> books ) {
+        Set<CartItem> cartItems = new HashSet<>();
+        
+        books.forEach( book -> {
+            CartItem cartItem = CartItem.builder()
+                .isbn( book.getId() )
+                .price( book.getPrice() )
+                .quantity( 1L )
+                .build();
+            cartItems.add( cartItem );
+        } );
+        return cartItems;
     }
 }
